a98c0e29e81df5e242343deb83def4de80340200
Your Name <you@example.com>
2019-02-28 13:33:21 +0700

    toantu

---
+++ toantu.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.writeln('Width: ',width);
+document.writeln('Height: ',height);
+document.write('area: ', area);
